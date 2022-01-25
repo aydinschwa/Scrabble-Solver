8ef0a40c1ccec8640bf403265d19b5a6b3db4517
Aydin <aydinschwa@gmail.com>
2022-01-24 22:28:25 -0800

    implement various cross-check bugfixes

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 from dawg import build_dawg, find_in_dawg
 import copy
+import time
 
 
 class Square:
@@ -79,77 +80,41 @@ class ScrabbleBoard:
              Square(), Square(modifier="2LS"), Square(), Square(), Square(modifier="3WS"),
              Square(sentinel=0)]
 
-        row_16 = [Square(sentinel=0) for _ in range(15)]
+        row_16 = [Square(sentinel=0) for _ in range(16)]
 
         self.board = [row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8,
                       row_9, row_10, row_11, row_12, row_13, row_14, row_15, row_16]
 
         self.dawg_root = dawg_root
 
+        self.words_on_board = []
+
         # variables to encode best word on a given turn
         self.word_score_dict = {}
         self.best_word = ""
         self.highest_score = 0
         self.dist_from_anchor = 0
+        # rows and columns of highest-scoring word found so far.
+        # these are the rows and columns of the tile already on the board
+        self.best_row = 0
+        self.best_col = 0
 
         # store squares that need updated cross-checks
         self.upper_cross_check = []
         self.lower_cross_check = []
 
-    def print_board(self):
-        print("    ", end="")
-        [print(str(num).zfill(2), end=" ") for num in range(1, 16)]
-        print()
-        for i, row in enumerate(self.board):
-            if i != 15:
-                print(str(i + 1).zfill(2), end="  ")
-            [print(square, end="  ") for square in row]
-            print()
-        print()
-
-    # method to insert words into board by row and column number
-    # using 1-based indexing for user input
-    def insert_word(self, row, col, word):
-        row -= 1
-        col -= 1
-        if len(word) + col > 15:
-            print(f'Cannot insert word "{word}" at column {col + 1}, '
-                  f'row {row + 1} not enough space')
-            return
-        curr_col = col
-        for i, letter in enumerate(word):
-            curr_square_letter = self.board[row][curr_col].letter
-            # if current square already has a letter in it, check to see if it's the same letter as
-            # the one we're trying to insert. If not, insertion fails, undo any previous insertions
-            if curr_square_letter:
-                if curr_square_letter == letter:
-                    curr_col += 1
-                else:
-                    print(f'Failed to insert letter "{letter}" of "{word}" at column {curr_col + 1}, '
-                          f'row {row + 1}. Square is occupied by letter "{curr_square_letter}"')
-                    self.upper_cross_check = []
-                    self.lower_cross_check = []
-                    for _ in range(i):
-                        curr_col -= 1
-                        self.board[row][curr_col].letter = None
-                    return
-            else:
-                # once letter is inserted, add squares above and below it to cross_check_queue
-                if row > 0:
-                    self.upper_cross_check.append((self.board[row - 1][curr_col], letter, row, curr_col))
-                if row < 15:
-                    self.lower_cross_check.append((self.board[row + 1][curr_col], letter, row, curr_col))
-
-                self.board[row][curr_col].letter = letter
-                curr_col += 1
-
     # transpose method that modifies self.board inplace
-    def transpose(self):
+    def _transpose(self):
         # https://datagy.io/python-transpose-list-of-lists/
         transposed_tuples = copy.deepcopy(list(zip(*self.board)))
         self.board = [list(sublist) for sublist in transposed_tuples]
 
+    # TODO: implement score multipliers
     def _score_word(self, word, dist_from_anchor):
+        # don't add words that are already on the board
+        if word in self.words_on_board:
+            return word, 0
+
         score = 0
         point_dict = {"A": 1, "B": 3, "C": 3, "D": 2,
                       "E": 1, "F": 4, "G": 2, "H": 4,
@@ -208,50 +173,10 @@ class ScrabbleBoard:
                     self._left_part(new_node, anchor_square_row, anchor_square_col, new_rack, new_word, limit - 1,
                                     dist_from_anchor + 1)
 
-    # gets all words that can be made using a selected filled square and the current word rack
-    def get_all_words(self, square_row, square_col, rack, word=""):
-        square_row -= 1
-        square_col -= 1
-
-        # reset word variables to clear out words from previous turns
-        self.word_score_dict = {}
-        self.best_word = ""
-        self.highest_score = 0
-
-        # get all words that start with the filled letter
-        self._extend_right(self.dawg_root, square_row, square_col, rack, word, 0)
-
-        # prevent generation of words with left parts that wouldn't fit on the board
-        limit = 6
-        if square_col < 7:
-            limit = square_col - 1
-
-        # try every letter in rack as possible anchor square
-        for i, letter in enumerate(rack):
-            temp_rack = rack[:i] + rack[i + 1:]
-            self.board[square_row][square_col - 1].letter = letter
-            self._left_part(self.dawg_root, square_row, square_col - 1, temp_rack, "", limit, 1)
-
-        # reset anchor square spot to blank after trying all combinations
-        self.board[square_row][square_col - 1].letter = None
-
-        self.insert_word(square_row + 1, square_col + 1 - self.dist_from_anchor, self.best_word)
-
-        # clear out cross-check lists before adding new words
-        self._update_cross_checks()
-
-        print(self.best_word)
-
     def _update_cross_checks(self):
         while self.upper_cross_check:
-            curr_square, lower_letter, curr_row, curr_col = self.upper_cross_check.pop()
+            curr_square, lower_letter, lower_row, lower_col = self.upper_cross_check.pop()
             chr_val = 65
-            # prevent horizontal row stacking greater than 2
-            if curr_square.letter:
-                self.board[curr_row - 2][curr_col].cross_checks = [0] * 26
-                self.board[curr_row + 1][curr_col].cross_checks = [0] * 26
-                continue
-
             for i, ind in enumerate(curr_square.cross_checks):
                 if ind == 1:
                     test_node = self.dawg_root.children[chr(chr_val)]
@@ -260,14 +185,8 @@ class ScrabbleBoard:
                 chr_val += 1
 
         while self.lower_cross_check:
-            curr_square, upper_letter, curr_row, curr_col = self.lower_cross_check.pop()
+            curr_square, upper_letter, upper_row, upper_col = self.lower_cross_check.pop()
             chr_val = 65
-            # prevent horizontal row stacking greater than 2
-            if curr_square.letter:
-                self.board[curr_row + 2][curr_col].cross_checks = [0] * 26
-                self.board[curr_row - 1][curr_col].cross_checks = [0] * 26
-                continue
-
             for i, ind in enumerate(curr_square.cross_checks):
                 if ind == 1:
                     test_node = self.dawg_root.children[upper_letter]
@@ -284,6 +203,143 @@ class ScrabbleBoard:
             chr_val += 1
         return False
 
+    def print_board(self):
+        print("    ", end="")
+        [print(str(num).zfill(2), end=" ") for num in range(1, 16)]
+        print()
+        for i, row in enumerate(self.board):
+            if i != 15:
+                print(str(i + 1).zfill(2), end="  ")
+            [print(square, end="  ") for square in row]
+            print()
+        print()
+
+    # method to insert words into board by row and column number
+    # using 1-based indexing for user input
+    def insert_word(self, row, col, word):
+        row -= 1
+        col -= 1
+        print(row, col, word)
+        if len(word) + col > 15:
+            print(f'Cannot insert word "{word}" at column {col + 1}, '
+                  f'row {row + 1} not enough space')
+            return
+        curr_col = col
+        for i, letter in enumerate(word):
+            curr_square_letter = self.board[row][curr_col].letter
+            # if current square already has a letter in it, check to see if it's the same letter as
+            # the one we're trying to insert. If not, insertion fails, undo any previous insertions
+            if curr_square_letter:
+                if curr_square_letter == letter:
+                    if row > 0:
+                        self.upper_cross_check.append((self.board[row - 1][curr_col], letter, row, curr_col))
+                    if row < 15:
+                        self.lower_cross_check.append((self.board[row + 1][curr_col], letter, row, curr_col))
+
+                    curr_col += 1
+                else:
+                    print(f'Failed to insert letter "{letter}" of "{word}" at column {curr_col + 1}, '
+                          f'row {row + 1}. Square is occupied by letter "{curr_square_letter}"')
+                    self.upper_cross_check = []
+                    self.lower_cross_check = []
+                    for _ in range(i):
+                        curr_col -= 1
+                        self.board[row][curr_col].letter = None
+                    return
+            else:
+                # once letter is inserted, add squares above and below it to cross_check_queue
+                if row > -1:
+                    self.upper_cross_check.append((self.board[row - 1][curr_col], letter, row, curr_col))
+                if row < 15:
+                    self.lower_cross_check.append((self.board[row + 1][curr_col], letter, row, curr_col))
+
+                self.board[row][curr_col].letter = letter
+                curr_col += 1
+
+        # place 0 cross-check sentinel at the beginning and end of inserted words to stop accidental overlap
+        if curr_col < 15:
+            self.board[self.best_row][curr_col].cross_checks = [0] * 26
+        if col - 1 > - 1:
+            self.board[self.best_row][col - 1].cross_checks = [0] * 26
+
+        self.words_on_board.append(word)
+
+        # TODO: makes shit weird
+        print(self.upper_cross_check)
+        print(self.lower_cross_check)
+        # self._update_cross_checks()
+
+    # gets all words that can be made using a selected filled square and the current word rack
+    def get_all_words(self, square_row, square_col, rack, word=""):
+        square_row -= 1
+        square_col -= 1
+
+        # get all words that start with the filled letter
+        self._extend_right(self.dawg_root, square_row, square_col, rack, word, 0)
+
+        # prevent generation of words with left parts that wouldn't fit on the board
+        limit = 6
+        if square_col < 7:
+            limit = square_col - 1
+
+        # create anchor square only if the space is empty
+        if self.board[square_row][square_col - 1].letter:
+            return
+
+        # try every letter in rack as possible anchor square
+        for i, letter in enumerate(rack):
+            temp_rack = rack[:i] + rack[i + 1:]
+            self.board[square_row][square_col - 1].letter = letter
+            self._left_part(self.dawg_root, square_row, square_col - 1, temp_rack, "", limit, 1)
+
+        # reset anchor square spot to blank after trying all combinations
+        self.board[square_row][square_col - 1].letter = None
+
+    # scan all tiles on board in both transposed and non-transposed state, find best move
+    def get_best_move(self, word_rack):
+
+        # clear out cross-check lists before adding new words
+        self._update_cross_checks()
+
+        # reset word variables to clear out words from previous turns
+        self.word_score_dict = {}
+        self.best_word = ""
+        self.highest_score = 0
+        self.best_row = 0
+        self.best_col = 0
+
+        transposed = False
+        for row in range(0, 15):
+            for col in range(0, 15):
+                curr_square = self.board[row][col]
+                if curr_square.letter and (not self.board[row][col - 1].letter):
+                    prev_best_word = self.best_word
+                    self.get_all_words(row + 1, col + 1, word_rack)
+                    if self.best_word != prev_best_word:
+                        self.best_row = row
+                        self.best_col = col
+
+        self._transpose()
+
+        for row in range(0, 15):
+            for col in range(0, 15):
+                curr_square = self.board[row][col]
+                if curr_square.letter and (not self.board[row][col - 1].letter):
+                    prev_best_word = self.best_word
+                    self.get_all_words(row + 1, col + 1, word_rack)
+                    if self.best_word != prev_best_word:
+                        transposed = True
+                        self.best_row = row
+                        self.best_col = col
+
+        if transposed:
+            self.insert_word(self.best_row + 1, self.best_col + 1 - self.dist_from_anchor, self.best_word)
+            self._transpose()
+
+        else:
+            self._transpose()
+            self.insert_word(self.best_row + 1, self.best_col + 1 - self.dist_from_anchor, self.best_word)
+
 
 big_list = open("lexicon/scrabble_words_complete.txt", "r").readlines()
 big_list = [word.strip("\n") for word in big_list]
@@ -291,18 +347,11 @@ root = build_dawg(big_list)
 
 word_rack = ["E", "O", "U", "C", "T", "R", "A"]
 scrabble_board = ScrabbleBoard(root)
-scrabble_board.insert_word(3, 7, "MORDANT")
-scrabble_board.transpose()
-scrabble_board.get_all_words(7, 3, word_rack)
-scrabble_board.get_all_words(8, 3, word_rack)
-scrabble_board.get_all_words(9, 3, word_rack)
-scrabble_board.get_all_words(10, 3, word_rack)
-scrabble_board.get_all_words(12, 3, word_rack)
+scrabble_board.insert_word(1, 7, "SIT")
 scrabble_board.print_board()
+for _ in range(50):
+    scrabble_board.get_best_move(word_rack)
+    print(scrabble_board.words_on_board)
+    scrabble_board.print_board()
 
-
-out = list(sorted(scrabble_board.word_score_dict.items(), key=lambda x: x[1], reverse=True))
-print(out)
-for word in out:
-    if not find_in_dawg(word[0], root):
-        raise Exception(f"Word generation incorrect: {word[0]} not in lexicon")
+print(scrabble_board.board[3][7].cross_checks)
